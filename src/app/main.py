8712bf353578fca70debdcfa5c7a6d147406bf61
DorLex <dorozhinskiysanya@gmail.com>
2026-01-11 00:41:05 +0300

    refact: all

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 
 from src.app.api.v1 import api_v1_router
+from src.common.logs.config import setup_logging
+
+setup_logging()
 
 app: FastAPI = FastAPI(title='Orders-App')
 
