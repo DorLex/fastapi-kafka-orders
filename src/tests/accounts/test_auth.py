07957266dbee1210a5605a968643fbaf307eb80e
DorLex <dorozhinskiysanya@gmail.com>
2026-01-13 22:32:45 +0300

    refact: all

--- src/tests/accounts/test_auth.py
+++ src/tests/accounts/test_auth.py
@@ -1,19 +1,24 @@
-# from fastapi import FastAPI
-# from httpx import AsyncClient
-# from starlette import status
-#
-#
-# async def test_login_by_access_token(app: FastAPI, client: AsyncClient):
-#     user_data = {
-#         'username': 'test_user_for_auth',
-#         'email': 'test-user-for-auth@test.com',
-#         'password': '123456789',
-#     }
-#
-#     url_registration = app.url_path_for('user_registration')
-#     await client.post(url_registration, json=user_data)
-#
-#     url_login = app.url_path_for('login_by_access_token')
-#     response = await client.post(url_login, data=user_data)
-#
-#     assert response.status_code == status.HTTP_200_OK, response.text
+from typing import TYPE_CHECKING
+
+from starlette import status
+from starlette.testclient import TestClient
+
+from src.app.bll.accounts.dto.user import UserResponseDTO
+
+if TYPE_CHECKING:
+    from httpx import Response
+
+
+async def test_login_by_access_token(client: TestClient, test_user: UserResponseDTO) -> None:
+    body: dict = {
+        'username': test_user.username,
+        'password': 'test_user_password',
+    }
+
+    url: str = '/api/v1/auth/token'
+
+    response: Response = client.post(url, json=body)
+    response_body: dict = response.json()
+
+    assert response.status_code == status.HTTP_200_OK, response.text
+    assert response_body.get('access_token')
--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,49 +1,63 @@
-# from typing import AsyncGenerator
-#
-# import pytest
-# from fastapi import FastAPI
-# from httpx import AsyncClient
-# from sqlalchemy.ext.asyncio import AsyncSession
-# from starlette import status
-#
-# from src.app.bll.accounts.dto import UserOutSchema
-#
-# from src.__dependencies import get_session
-# from src.app.main import app as main_app
-#
-#
-# @pytest.fixture(scope='session', autouse=True)
-# async def prepare_db():
-#     assert MODE == 'TEST'
-#
-#     async with async_engine_test.begin() as conn:
-#         await conn.run_sync(Base.metadata.create_all)
-#
-#     yield
-#
-#     async with async_engine_test.begin() as conn:
-#         await conn.run_sync(Base.metadata.drop_all)
-#
-#
-# async def override_get_session() -> AsyncGenerator[AsyncSession, None]:
-#     async with SessionTest() as session:
-#         yield session
-#
-#
-# main_app.dependency_overrides[get_db] = override_get_session
-#
-#
-# @pytest.fixture(scope='session')
-# def app():
-#     return main_app
-#
-#
-# @pytest.fixture(scope='session')
-# async def client(app: FastAPI) -> AsyncGenerator[AsyncClient, None]:
-#     async with AsyncClient(app=app, base_url='http://test') as async_client:
-#         yield async_client
-#
-#
+from collections.abc import AsyncGenerator, Generator
+from typing import Any
+
+import pytest
+from alembic import command
+from alembic.config import Config
+from sqlalchemy import NullPool
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, async_sessionmaker, create_async_engine
+from starlette.testclient import TestClient
+
+from src.app.bll.accounts.dto.user import UserCreateDTO, UserResponseDTO
+from src.app.bll.accounts.services.user import UserService
+from src.app.dal.accounts.repositories.user import UserRepository
+from src.app.main import app
+from src.common.constants.paths import BASE_DIR
+from src.common.db.dependencies import get_db
+from src.common.envs import env_config
+
+alembic_config: Config = Config(BASE_DIR / 'alembic.ini')
+
+env_config.postgres_db = 'test_orders_db'
+
+
+@pytest.fixture(scope='session', autouse=True)
+def setup() -> Generator[None, Any]:
+    if env_config.postgres_db == 'test_orders_db':
+        command.upgrade(alembic_config, 'head')  # накатываем миграции на тестовую БД
+
+        yield  # тут контекст переключается на прогон всех тестов
+
+        command.downgrade(alembic_config, 'base')  # откатываем миграции на тестовой БД
+
+    else:
+        raise RuntimeError('Неверные настройки тестов!')
+
+
+async_test_engine: AsyncEngine = create_async_engine(
+    env_config.postgresql_url,
+    poolclass=NullPool,  # иначе падает ошибка RuntimeError: ... attached to a different loop
+)
+
+TestAsyncSessionMaker: async_sessionmaker[AsyncSession] = async_sessionmaker(
+    async_test_engine,
+    expire_on_commit=False,
+)
+
+
+async def override_get_db() -> AsyncGenerator[AsyncSession]:
+    async with TestAsyncSessionMaker() as session:
+        yield session
+
+
+app.dependency_overrides[get_db] = override_get_db  # подменяем зависимость основной сессии БД на тестовую
+
+
+@pytest.fixture
+def client() -> TestClient:
+    return TestClient(app)
+
+
 # @pytest.fixture(scope='session')
 # async def base_test_user_data():
 #     return {
@@ -51,18 +65,24 @@
 #         'email': 'base-test-user@test.com',
 #         'password': '123456789',
 #     }
-#
-#
-# @pytest.fixture(scope='session')
-# async def base_test_user(app: FastAPI, prepare_db, client: AsyncClient, base_test_user_data):
-#     url = app.url_path_for('user_registration')
-#     response = await client.post(url, json=base_test_user_data)
-#
-#     assert response.status_code == status.HTTP_201_CREATED, response.text
-#
-#     return UserOutSchema(**response.json())
-#
-#
+
+
+@pytest.fixture(scope='session')
+async def test_user() -> UserResponseDTO:
+    async with TestAsyncSessionMaker() as db:
+        user_service: UserService = UserService(UserRepository(db))
+        user: UserResponseDTO = await user_service.create_user(
+            UserCreateDTO(
+                username='test_user',
+                email='test_user@email.com',
+                password='test_user_password',
+            ),
+        )
+        await db.commit()
+
+    return user
+
+
 # @pytest.fixture(scope='session')
 # async def access_token(app: FastAPI, base_test_user, client: AsyncClient, base_test_user_data):
 #     url = app.url_path_for('login_by_access_token')
@@ -76,3 +96,8 @@
 # @pytest.fixture(scope='session')
 # async def auth_headers(access_token):
 #     return {'Authorization': f'Bearer {access_token}'}
+
+
+# @pytest.fixture(scope='session')
+# def app():
+#     return main_app
