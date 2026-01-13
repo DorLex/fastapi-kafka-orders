a47a538fe8ec46dcdf21fb9da4f81b3fcaaee46d
DorLex <dorozhinskiysanya@gmail.com>
2026-01-13 23:12:59 +0300

    refact: all

--- src/tests/accounts/test_users.py
+++ src/tests/accounts/test_users.py
@@ -3,6 +3,8 @@ from typing import TYPE_CHECKING
 from starlette import status
 from starlette.testclient import TestClient
 
+from src.app.bll.accounts.dto.user import UserResponseDTO
+
 if TYPE_CHECKING:
     from httpx import Response
 
@@ -16,13 +18,16 @@ class TestUsers:
         assert response.status_code == status.HTTP_200_OK, response.text
         assert len(response_body) > 0
 
-    # async def test_read_users_me(self, client: TestClient, auth_headers, base_test_user_data) -> None:
-    #     url: str = '/api/v1/...'
-    #     response = client.get(url, headers=auth_headers)
-    #
-    #     assert response.status_code == status.HTTP_200_OK, response.text
-    #     assert response.json().get('username') == base_test_user_data.get('username')
-    #
+    async def test_get_user_me(self, client: TestClient, auth_headers: dict, base_test_user: UserResponseDTO) -> None:
+        url: str = '/api/v1/users/me'
+        response: Response = client.get(url, headers=auth_headers)
+        response_body: dict = response.json()
+
+        assert response.status_code == status.HTTP_200_OK, response.text
+        assert response_body.get('id') == base_test_user.id
+        assert response_body.get('username') == base_test_user.username
+        assert response_body.get('email') == base_test_user.email
+
     # async def test_read_users_with_orders(self, client: TestClient, auth_headers) -> None:
     #     url: str = '/api/v1/...'
     #     response = client.get(url, headers=auth_headers)
